bdf3c9a307480f371e41f415d5373f04c8ab0018
liam <thurainkyawzawl55@gmail.com>
2025-02-19 10:42:29 +0630

    change name

--- src/nav.js
+++ src/nav.js
@@ -2,7 +2,7 @@ import spider from "../public/spider.png";
 const nav = () => {
   return `
      <nav class="container flex justify-between items-center mx-auto py-4">
-     <button class="font-semibold text-2xl text-white cursor-pointer hover:bg-[#424242] transition-colors duration-100 rounded-md px-4 py-2">ChatGPT</button>
+     <button class="font-semibold text-2xl text-white cursor-pointer hover:bg-[#424242] transition-colors duration-100 rounded-md px-4 py-2">LiamGPT</button>
      <button class="border border-dashed rounded-full border-white p-1">
         <img src="${spider}" class="size-8 rounded-full object-cover">
      </button>
