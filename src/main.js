b686a1febf5e9c518e9afa4a2181bcecc6ebae7d
liam <thurainkyawzawl55@gmail.com>
2025-02-19 10:19:36 +0630

    change send

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ const button1 = document.getElementById("button1");
 const chat_content = document.getElementById("chat_content");
 form1.addEventListener("submit", async (event) => {
   event.preventDefault();
+  if (area1.value.length < 2) return;
   button1.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"><animateTransform attributeName="transform" type="rotate" dur="0.75s" values="0 12 12;360 12 12" repeatCount="indefinite"/></path></svg>`;
   button1.disabled = true;
   button1.classList.add("pointer-events-none");
